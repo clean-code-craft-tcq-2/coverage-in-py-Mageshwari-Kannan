35f83d111bc8e3ef42e91bebfee47f6f95d305bb
Mageshwari Kannan <91052034+Mageshwari-Kannan@users.noreply.github.com>
2022-02-21 20:22:42 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -8,6 +8,14 @@ class TypewiseTest(unittest.TestCase):
         self.assertTrue(typewise_alert.infer_breach(110, 50, 100) == 'TOO_HIGH')
     def test_infers_breach_as_per_limits(self):
         self.assertTrue(typewise_alert.infer_breach(80, 50, 100) == 'NORMAL')
-        
+    def test_infers_breach_as_per_limits(self):
+        self.assertTrue(typewise_alert.infer_breach(80, 50, 100) == 'NORMAL')
+    def test_classify_temperature_breach_for_PASSIVE_COOLING(self):
+        self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 35})	
+    def test_classify_temperature_breach_for_HI_ACTIVE_COOLING(self):
+        self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 45})	  
+    def test_classify_temperature_breach_for_MED_ACTIVE_COOLING(self):
+        self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 40})		
+    
 if __name__ == '__main__':
   unittest.main()

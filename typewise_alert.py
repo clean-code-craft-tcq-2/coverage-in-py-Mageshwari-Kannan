dc8a734c55b96fd79c972e0c3545f5d62fcb3c48
Mageshwari Kannan <91052034+Mageshwari-Kannan@users.noreply.github.com>
2022-02-21 16:50:27 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,5 +1,5 @@
-send_email_receipent = { recepient  :  'a.b@c.com',
-						 message : {'TOO_LOW'  :  'Hi, the temperature is too low',
+send_email_receipent = { 'recepient'  :  'a.b@c.com',
+						 'message' : {'TOO_LOW'  :  'Hi, the temperature is too low',
 						 'TOO_HIGH' :  'Hi, the temperature is too high' } }
 
 def infer_breach(value, lowerLimit, upperLimit):
@@ -8,15 +8,16 @@ def infer_breach(value, lowerLimit, upperLimit):
   if value > upperLimit:
     return 'TOO_HIGH'
   return 'NORMAL'
-	
+
+
 def classify_temperature_breach(coolingType, temperatureInC):
 	lowerLimit = 0
 	cooling_limit_dict = { 'PASSIVE_COOLING'    : 35 ,
 						   'HI_ACTIVE_COOLING'  : 45 , 
 						   'MED_ACTIVE_COOLING' :  40 }	
-	for cooling_value in cooling_limit_dict.keys():
+	for cooling_limit in cooling_limit_dict.keys():
 		if cooling_value == coolingType : 
-			upperLimit = cooling_limit_dict[cooling_value]
+			upperLimit = cooling_limit_dict[cooling_limit]
 			return infer_breach(temperatureInC, lowerLimit, upperLimit)
 		else :
 			not_in_range = { 'lowerLimit' : 'Not in range', "upperLimit" : 'Not in range'}
@@ -33,28 +34,26 @@ def GetBreachType(batteryChar, temperatureInC):
 		return classify_temperature_breach(batteryChar, temperatureInC)	
 	else:
 		return 	'ERROR' 
-		
+  
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType = GetBreachType(batteryChar, temperatureInC) 
-	if breachType!='ERROR' :
-		alert = alertTarget(breachType)
-	else:
-		False 
-	return(breachType)
+  if breachType!='ERROR':
+    alert = alertTarget(breachType)
+  else:
+    False 
+  return(breachType)
 
-def send_to_controller(breachType): 
+def send_to_controller(breachType):
 	header = 0xfeed 
 	command_to_controller = (f'{header}, {breachType}') 
 	print(command_to_controller) 
 	return(command_to_controller) 
-	
+
 def Generate_email_content(breachtype, email_messages): 
 	return email_messages[breachtype] 
-	
-def send_to_email(breachType): 
+
+def send_to_email(breachType):
 	mail_content = Generate_email_content(breachType, send_email_receipent['messages']) 
 	sending_email = f"To: {send_email_receipent['recepient']} : {mail_content}" 
 	print(sending_email) 
 	return(sending_email) 
-
-

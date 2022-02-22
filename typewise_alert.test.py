4ecfb436b36c751c14ad8c2f018032cdc6b7aeef
Mageshwari Kannan <91052034+Mageshwari-Kannan@users.noreply.github.com>
2022-02-22 13:57:04 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -23,9 +23,7 @@ class TypewiseTest(unittest.TestCase):
     def test_classify_temperature_breach_for_NORMAL(self):
         self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 36)=='NORMAL')  
     def test_classify_temperature_breach_for_TOO_HIGH(self):
-        self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING', 47)=='TOO_HIGH')    
-    def test_classify_temperature_breach_for_TOO_HIGH(self):
-        self.assertTrue(typewise_alert.classify_temperature_breach('NO_COOLING', 80)=='WARNING')
+        self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING', 47)=='TOO_HIGH') 
 
         
 if __name__ == '__main__':

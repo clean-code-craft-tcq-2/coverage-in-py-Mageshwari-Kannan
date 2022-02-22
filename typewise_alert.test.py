61276f3a2cdabf78d558462fe3e9b2a9b861441c
Mageshwari Kannan <91052034+Mageshwari-Kannan@users.noreply.github.com>
2022-02-22 10:47:42 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -14,21 +14,21 @@ class TypewiseTest(unittest.TestCase):
     def test_classify_temperature_breach_for_PASSIVE_COOLING(self):
         self.assertTrue(typewise_alert.classify_temperature_breach(100, 'PASSIVE_COOLING') == 'TOO_HIGH')	
     def test_classify_temperature_breach_for_PASSIVE_COOLING(self):
-        self.assertTrue(typewise_alert.classify_temperature_breach(15, 'PASSIVE_COOLING') == 'NORMAL')	
+        self.assertTrue(typewise_alert.classify_temperature_breach(10, 'PASSIVE_COOLING') == 'NORMAL')	
         
     def test_classify_temperature_breach_for_HI_ACTIVE_COOLING(self):
         self.assertTrue(typewise_alert.classify_temperature_breach(-15, 'HI_ACTIVE_COOLING') == 'TOO_LOW')	
     def test_classify_temperature_breach_for_HI_ACTIVE_COOLING(self):
         self.assertTrue(typewise_alert.classify_temperature_breach(120, 'HI_ACTIVE_COOLING') == 'TOO_HIGH')
     def test_classify_temperature_breach_for_HI_ACTIVE_COOLING(self):
-        self.assertTrue(typewise_alert.classify_temperature_breach(40, 'HI_ACTIVE_COOLING') == 'NORMAL')
+        self.assertTrue(typewise_alert.classify_temperature_breach(26, 'HI_ACTIVE_COOLING') == 'NORMAL')
         
     def test_classify_temperature_breach_for_MED_ACTIVE_COOLING(self):
         self.assertTrue(typewise_alert.classify_temperature_breach(-5, 'MED_ACTIVE_COOLING') == 'TOO_LOW')		
     def test_classify_temperature_breach_for_MED_ACTIVE_COOLING(self):
         self.assertTrue(typewise_alert.classify_temperature_breach(110, 'MED_ACTIVE_COOLING') == 'TOO_HIGH')	
     def test_classify_temperature_breach_for_MED_ACTIVE_COOLING(self):
-        self.assertTrue(typewise_alert.classify_temperature_breach(36, 'MED_ACTIVE_COOLING') == 'NORMAL')	
+        self.assertTrue(typewise_alert.classify_temperature_breach(20, 'MED_ACTIVE_COOLING') == 'NORMAL')	
         
 if __name__ == '__main__':
   unittest.main()

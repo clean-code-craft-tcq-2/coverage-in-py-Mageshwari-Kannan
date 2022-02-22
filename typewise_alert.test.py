f72287601433d34b1666252cb5212123bc30a6eb
Mageshwari Kannan <91052034+Mageshwari-Kannan@users.noreply.github.com>
2022-02-22 13:21:09 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -9,13 +9,13 @@ class TypewiseTest(unittest.TestCase):
     def test_infers_breach_as_per_limits(self):
         self.assertTrue(typewise_alert.infer_breach(80, 50, 100) == 'NORMAL')
     def test_classify_temperature_breach_for_PASSIVE_COOLING(self):
-        self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 35})
+        self.assertTrue(typewise_alert.coolingtype_range('PASSIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 35})
     def test_classify_temperature_breach_for_HI_ACTIVE_COOLING(self):
-        self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 45})
+        self.assertTrue(typewise_alert.coolingtype_range('HI_ACTIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 45})
     def test_classify_temperature_breach_for_MED_ACTIVE_COOLING(self):
-        self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 40})
+        self.assertTrue(typewise_alert.coolingtype_range('MED_ACTIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 40})
     def test_classify_temperature_breachs_for_WRONG_KEY(self):
-        self.assertTrue(typewise_alert.classify_temperature_breach('WRONG_KEY') == {"lowerLimit" : 'Not in limits', "upperLimit" : 'Not in limits'})
+        self.assertTrue(typewise_alert.coolingtype_range('WRONG_KEY') == {"lowerLimit" : 'Not in limits', "upperLimit" : 'Not in limits'})
     def test_classify_temperature_breach_for_NORMAL(self):
         self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 38)=='NORMAL')
     def test_classify_temperature_breach_for_TOO_LOW(self):
